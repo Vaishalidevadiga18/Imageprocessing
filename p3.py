cfb5cb5431b4b2bdf2c5f3582f147352efaa26f2
vaishali <vaishalidevadiga81@gmail.com>
2025-03-13 11:48:44 +0530

    Initial commit

---
+++ p3.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+def load_image(path):
+    image=cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        print("Error!Could not find a path")
+        exit()
+    return image
+def apply_sobel(image):
+    sobelx=cv2.Sobel(image,cv2.CV_64F,1,0,ksize=3)
+    sobely=cv2.Sobel(image,cv2.CV_64F,0,1,ksize=3)
+    sobel_combined=cv2.magnitude(sobelx,sobely)
+    return sobel_combined
+
+def apply_laplacian(image):
+    laplacian=cv2.Laplacian(image,cv2.CV_64F)
+    return laplacian
+
+def apply_canny(image,threshold1,threshold2):
+    canny=cv2.Canny(image,threshold1,threshold2)
+    return canny
+
+def main():
+    image_path=input("enter image path:")
+    image=load_image(image_path)
+    
+    sobel_edges=apply_sobel(image)
+    laplacian_edges=apply_laplacian(image)
+    canny_edges=apply_canny(image,100,200)
+    
+    cv2.imshow('Original Image',image)
+    cv2.imshow('Sobel Edges ',np.uint8(sobel_edges))
+    cv2.imshow('Laplacian_edges',np.uint8(laplacian_edges))
+    cv2.imshow('Canny Edges',canny_edges)
+    
+    save_option=input("do you want to save processed images?(yes/no):".strip().lower())
+    if save_option=="yes":
+        cv2.imwrite("sobel_edges.jpg",np.uint8(sobel_edges))
+        cv2.imwrite('laplacian_edges.jpg',np.uint8(laplacian_edges))
+        cv2.imwrite('canny_edges.jpg',canny_edges)
+        print('Processed image saved sucessfully!')
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+if __name__=="__main__":
+    main()
